b6c33b7c944e130c34cae4a70f29ecff91810e09
ankit-gautam23 <ankit.gautam23@gmail.com>
2023-04-09 14:39:59 +0530

    Added to main method and Block class

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,27 @@
+import java.security.Security;
+import java.util.ArrayList;
+import com.google.gson.GsonBuilder;
+import org.bouncycastle.*;
+
 public class Main {
+    public static ArrayList<Block> blockChain = new ArrayList<Block>();
+    public static int difficulty = 5;
+    public static Wallet walletA;
+    public static Wallet walletB;
+
     public static void main(String[] args) {
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        Wallet walletA = new Wallet();
+        Wallet walletB = new Wallet();
 
+        System.out.println("Private and Public Keys : ");
+        System.out.println(StringUtil.getStringFromKey(walletA.privateKey));
+        System.out.println(StringUtil.getStringFromKey(walletA.publicKey));
 
+        Transaction transaction = new Transaction(walletA.publicKey, walletB.publicKey, 5, null);
+        transaction.generateSignature(walletA.privateKey);
+
+        System.out.println("Is Signature Verified");
+        System.out.println(transaction.verifySignature());
     }
 }
