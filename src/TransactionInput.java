d8661faf091d5d1a74c787633ab42a612831cb56
ankit-gautam23 <ankit.gautam23@gmail.com>
2023-04-09 13:46:52 +0530

    Implemented TransactionInput and TransactionOutput classes.

--- src/TransactionInput.java
+++ src/TransactionInput.java
@@ -1,2 +1,8 @@
 public class TransactionInput {
+    public String transactionOutputId;
+    public TransactionOutput UTXO;
+
+    public TransactionInput(String transactionOutputId){
+        this.transactionOutputId = transactionOutputId;
+    }
 }
--- src/TransactionOutput.java
+++ src/TransactionOutput.java
@@ -1,2 +1,20 @@
+import java.security.PublicKey;
+
 public class TransactionOutput {
+    public String id;
+    public PublicKey reciever;
+    public float value;
+    public String parentTransactionId;
+
+
+    public TransactionOutput(PublicKey reciever,float value, String parentTransactionId){
+        this.reciever = reciever;
+        this.value = value;
+        this.parentTransactionId = parentTransactionId;
+        this.id = StringUtil.applySha256(StringUtil.getStringFromKey(reciever)+Float.toString(value)+parentTransactionId);
+
+    }
+    public boolean isMine(PublicKey publicKey){
+        return (publicKey == reciever);
+    }
 }
